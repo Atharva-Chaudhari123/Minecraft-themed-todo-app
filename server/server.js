ca43f32faf846cadf6f3f4392a79ccc9b34cb38c
Atharva-Chaudhari123 <atharvachaudhari4444@gmail.com>
2025-01-14 00:55:54 +0530

    efforts paid of

--- server/server.js
+++ server/server.js
@@ -53,10 +53,10 @@ const userSchema = mongoose.Schema({
 
 const getTaskModel = (collection_name, userSchema) =>{
     if(!userSchema   ){
-        return mongoose.model(collection_name,dbSchema) ;
+        return mongoose.model(collection_name, dbSchema , collection_name) ;
 
     }else{
-        return mongoose.model (collection_name, userSchema) ;
+        return mongoose.model (collection_name, userSchema , collection_name) ;
     }
 }
  
@@ -64,16 +64,17 @@ const getTaskModel = (collection_name, userSchema) =>{
 
 app.post("/task/:id" , async(req, res) =>{
 
-    const Task = getTaskModel(req.body.username) ;
+    const Task = getTaskModel(req.params.id) ;
     
 
-    console.log(username)
+    console.log(req.params.id) ;
+    console.log(req.body.username) ;
 
     const result = await Task.create({
         task : req.body.task ,
         status : req.body.status,
         date :  Date.now() ,
-        _id : `${req.params.username}-${Date.now()}`  
+        _id : `${req.body.username}-${Date.now()}`  
     })
     console.log("below db operation");
     return res.status(200).end("what uppp") ;
@@ -104,20 +105,40 @@ app.post("/newuser", async(req,res)=>{
 
 }) ;
 
-app.post("/search", async(req, res)=>{
-    const user = getTaskModel("users", userSchema) 
-    username = req.body.username ;
-    password = req.body.password ;
-   const bool =  await user.findOne({username, password}) ; 
-   if(bool) {
-    return res.send(true)
-   }
-   else{
-    return res.send(false)
-   }
+app.post("/search", async (req, res) => {
+    const user = getTaskModel("users", userSchema);
+    const username = String(req.body.username); // Ensure it's a string
+    const password = String(req.body.password);
+
+    console.log("Querying with:", { username, password });
+
+    try {
+        const bool = await user.findOne({ username, password });
+        console.log("Query result:", bool);
+
+        if (bool != null) {
+            console.log("returned true")
+            return res.end("true");
+        } else {
+            console.log("returned false")
+            return res.end("false"); // Return "false" as a string
+        }
+    } catch (err) {
+        console.error("Error during database query:", err);
+        return res.status(500).end("Internal server error");
+    }
+});
+
+app.get("/fetchtasks/:id", async(req, res)=>{
+    const username = req.params.id ;
+    const model = getTaskModel(username) ;
+
+    const allTasks = await model.find({}) ;
+    console.log(allTasks)
+
+    return res.json(allTasks) ;
 
 })
-
 app.listen(port, (err) =>{
     if(err){   
         return console.log("Error : " + err) ;
